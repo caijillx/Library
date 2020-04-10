c8c05f9b352af788e8b5ba884ac4d02cb0b08bc0
llx <463264977@qq.com>
2020-04-10 19:58:48 +0800

    1

--- LibraryApp/models.py
+++ LibraryApp/models.py
@@ -1,29 +1,74 @@
 # 定义模型
+# coding: utf-8
 from datetime import datetime
 from LibraryApp import db
-# class Admin(db.Model):
-#     __tablename__="admin"
-#
-# class Book(db.Model):
-#     __tablename__ = "book"
-#     pass
-#
-# class BookInfo(db.Model):
-#     __tablename__ = "book_info"
-#     pass
-#
-# class BorrwoInfo(db.Model):
-#     __tablename__ = "borrow_info"
-#     pass
-#
-# class Reader(db.Model):
-#     __tablename__ = "reader"
-#     pass
-#
-# class ReserveInfo(db.Model):
-#     __tablename__ = "reserve_info"
-#     pass
 
-if __name__ == "__main__":
-    pass
-    # db.creatall() # 会自动创建数据表
+class Admin(db.Model):
+    __tablename__ = 'admin'
+
+    user_id = db.Column(db.String(30), primary_key=True)
+    user_name = db.Column(db.String(30))
+    pswd = db.Column(db.String(30))
+
+
+class Reader(db.Model):
+    __tablename__ = 'reader'
+
+    id = db.Column(db.String(30), primary_key=True)
+    name = db.Column(db.String(30))
+    phone = db.Column(db.String(30))
+    Email = db.Column(db.String(30))
+
+
+class Book(db.Model):
+    __tablename__ = 'book'
+
+    isbn = db.Column(db.String(30), primary_key=True)
+    name = db.Column(db.String(30))
+    author = db.Column(db.String(30))
+    publisher = db.Column(db.String(30))
+    pub_date = db.Column(db.String)
+    amount = db.Column(INTEGER(11))
+    agent_id = db.Column(db.ForeignKey('admin.user_id'), index=True)
+
+    agent = db.relationship('Admin')
+
+
+class BookInfo(db.Model):
+    __tablename__ = 'book_info'
+
+    book_id = db.Column(db.String(30), primary_key=True)
+    isbn = db.Column(db.ForeignKey('book.isbn'), index=True)
+    location = db.Column(db.String(30))
+    status = db.Column(db.String(30))
+
+    book = db.relationship('Book')
+
+
+class ReserveInfo(db.Model):
+    __tablename__ = 'reserve_info'
+
+    reader_id = db.Column(db.ForeignKey('reader.id'), primary_key=True, nullable=False)
+    isbn = db.Column(db.ForeignKey('book.isbn'), primary_key=True, nullable=False, index=True)
+    reserve_date = db.Column(db.String, primary_key=True, nullable=False)
+    status = db.Column(db.String(30))
+    agent_id = db.Column(db.ForeignKey('admin.user_id'), index=True)
+
+    agent = db.relationship('Admin')
+    book = db.relationship('Book')
+    reader = db.relationship('Reader')
+
+
+class BorrowInfo(db.Model):
+    __tablename__ = 'borrow_info'
+
+    reader_id = db.Column(db.ForeignKey('reader.id'), primary_key=True, nullable=False)
+    book_id = db.Column(db.ForeignKey('book_info.book_id'), primary_key=True, nullable=False, index=True)
+    borrow_date = db.Column(db.String, primary_key=True, nullable=False)
+    due_date = db.Column(db.String)
+    return_date = db.Column(db.String)
+    agent_id = db.Column(db.ForeignKey('admin.user_id'), index=True)
+
+    agent = db.relationship('Admin')
+    book = db.relationship('BookInfo')
+    reader = db.relationship('Reader')
