d7b19cb503459b49b02b7e670f365ec427db3473
llx <463264977@qq.com>
2020-04-24 10:44:18 +0800

    ysnb

--- LibraryApp/views.py
+++ LibraryApp/views.py
@@ -7,11 +7,38 @@ import datetime
 import os
 
 
+# 导航界面
 @app.route('/')
 def index():
     admin = Admin.query.filter(Admin.user_name=="ys").first()
     return "<h1>欢迎来到主页！{}</h1>".format(admin.user_name)
 
+
+# 登录界面
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+
+# 入库管理界面
+@app.route('/putin')
+def login():
+    return render_template('putin.html')
+
+
+# 借书界面
+@app.route('/borrowbook')
+def login():
+    return render_template('borrowbook.html')
+
+
+# 预约界面
+@app.route('/orderbook')
+def login():
+    return render_template('orderbook.html')
+
+
+# 还书界面
+@app.route('/returnbook')
+def login():
+    return render_template('returnbook.html')
