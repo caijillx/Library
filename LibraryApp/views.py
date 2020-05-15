6d841abcdde8d1bb78f81e109e7fd36da1f847d7
luziyi1998 <252749799@qq.com>
2020-05-15 18:17:20 +0800

    commit不算上传吧？

--- LibraryApp/views.py
+++ LibraryApp/views.py
@@ -1,7 +1,7 @@
 # 定义视图
 from LibraryApp import app
 
-from flask import render_template, redirect, session, url_for, flash, request
+from flask import render_template, redirect, session, url_for, flash, request,jsonify
 from LibraryApp.forms import LoginForm
 from LibraryApp.models import *
 from functools import wraps
@@ -61,10 +61,85 @@ def putin():
 
 
 # 借书界面
-@app.route('/borrowbook')
+
+@app.route('/borrowbook',methods=['POST','GET'])
 @admin_login_req
 def borrowbook():
-    return render_template('borrowbook.html')
+    if request.method == 'GET': #处理get请求
+        bookisbn = request.args.get('bookisbn')
+        print('isbn=', bookisbn)
+        if bookisbn == None:    #isbn为空，则显示所有书的信息
+            book_list = Book.query.all()
+            print('book_list', book_list)
+            book_list_dict = []
+            for book in book_list:
+                print(book.isbn)
+                book_list_dict.append(
+                    {
+                        'isbn': book.isbn,
+                        'name': book.name
+                    }
+                )
+            # print(book_list_dict)
+            # book_list_json = json.dumps(book_list_dict)
+
+            #return book_list_json
+            return render_template('borrowbook.html',books=book_list_dict)
+        book_info = BookInfo.query.filter_by(isbn=bookisbn).all() # isbn不为空，返回具体的书本状态
+        bookinfo_list_dict = []
+        for book in book_info:
+            print(book.isbn)
+            bookinfo_list_dict.append(
+                {
+                    'book_id': book.book_id,
+                    'isbn': book.isbn,
+                    'location': book.location,
+                    'status': book.status
+                }
+            )
+        return render_template('borrowbook.html', bookinfo=bookinfo_list_dict)
+    else:   #处理post请求
+        book_id = request.form.get('book_id')
+        isbn = request.form.get('isbn')
+        id = request.form.get('id')
+        user_id = session['user_id'] #session中取经办人id
+        book_info = BookInfo.query.filter_by(isbn=isbn).all()  # isbn不为空，返回具体的书本状态
+        bookinfo_list_dict = []
+        for book in book_info:
+            print(book.isbn)
+            bookinfo_list_dict.append(
+                {
+                    'book_id': book.book_id,
+                    'isbn': book.isbn,
+                    'location': book.location,
+                    'status': book.status
+                }
+            )
+        print("book_id:"+book_id)
+        print("isbn:"+isbn)
+        print("id:"+id)
+        print("user_id:"+user_id)
+        id = Reader.query.filter_by(id=id).first()
+        if id != None:
+            #插入借书记录
+            BorrowInfo.query.all()
+            borrow_date = datetime.datetime.now().strftime("%Y-%m-%d")  # 当前时间 Y-m-d
+            due_date = (datetime.datetime.now() + datetime.timedelta(days=60)).strftime("%Y-%m-%d")  # 60天后
+            # borroInfo = BorrowInfo(reader_id=id,book_id=book_id,borrow_date=borrow_date,due_date=due_date,agent_id=user_id)
+            # db.session.add(borroInfo)
+            # db.session.commit()
+            # 上面的借书记录插入数据库写不通！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+            # 除了插入借书记录还需要更改书的状态
+            # 所需数据已给出：isbn/book_id
+            return jsonify({"success": 1001, "msg": "借阅成功"})#由borrowbook的ajax接受，弹窗显示
+        else:
+            #id在数据库中找不到
+            return jsonify({"error": 201, "msg": "没有这个id!"})
+
+        # return render_template('borrowbook.html', bookinfo=bookinfo_list_dict)
+
+
+
 
 
 # 预约界面
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from LibraryApp import app
 from flask_script import Manager
-
+from LibraryApp.models import *
 manager = Manager(app)
 
 if __name__ == "__main__":
+    # reader = Reader(id="17121385",name="mmmm",phone="111111",Email="15151@qq.com")
+    # db.session.add(reader)
+    # db.session.commit()
     manager.run()
