0c23bb8ba392b2d0a78eca5721ba6165fc27a0cb
luziyi1998 <252749799@qq.com>
2020-05-21 22:27:31 +0800

    ls叫我push

--- main.py
+++ main.py
@@ -4,7 +4,5 @@ from LibraryApp.models import *
 manager = Manager(app)
 
 if __name__ == "__main__":
-    # reader = Reader(id="17121385",name="mmmm",phone="111111",Email="15151@qq.com")
-    # db.session.add(reader)
-    # db.session.commit()
+
     manager.run()
